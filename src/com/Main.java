dd4728af53624f8ba0ca1443337821a4cd26e51d
dzomboallan <dzomboallan1809@gmail.com>
2022-06-23 03:17:18 +0300

    Initial commit

---
+++ src/com/Main.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Main {
+    public static void main (String [] args){
+
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.insert(50);
+        numbers.removeAt(0);
+        //System.out.println(numbers.indexOf(50));
+
+        numbers.print();
+
+    }
+}
